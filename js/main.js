d7e4725fcb95ad475835bef75b9ba72981703c0c
cperion <cedric.perion@ensg.eu>
2019-11-30 22:39:35 +0100

    ajouts des marqueurs

--- js/main.js
+++ js/main.js
@@ -17,12 +17,82 @@ var marker_cnam = L.marker([48.867008, 2.355349]);
 marker_cnam.bindPopup("Conservatoire National des Arts et Métiers").openPopup();
 marker_cnam.on("click", clickOnCNAM);
 
+// Deuxieme marqueur : Rome
+
+var marker_rome= L.marker([41.8941, 12.4854]);
+marker_rome.bindPopup("Forum Romain").openPopup();
+marker_rome.on("click", clickOnROME);
+ 
+// Troisieme marqueur : Venise
+
+var marker_venise = L.marker([41.4385, 12.3275]);
+marker_venise.bindPopup("Venise").openPopup();
+marker_venise.on("click", clickOnVENISE);
+
+// Quatrième marqueur : Rosslyn
+
+var marker_rosslyn = L.marker([55.8560, -3.1602]);
+marker_rosslyn.bindPopup("Rosslyn").openPopup();
+marker_rosslyn.on("click", clickOnROSSLYN);
+
+// Cinquième marqueur : Jérusalem
+
+var marker_jerusalem = L.marker([31.7797, 35.2295]);
+marker_jerusalem.bindPopup("Jerusalem").openPopup();
+marker_jerusalem.on("click", clickOnJERUSALEM);
+
+// Sixième marqueur : Newport
+
+var marker_newport = L.marker([41.4867, -71.3099]);
+marker_newport.bindPopup("Newport").openPopup();
+marker_newport.on("click", clickOnNEWPORT);
+
+// Septieme marqueur : Herjolfsnes
+
+var marker_herjolfsnes = L.marker([-44.4666, 59.9833]);
+marker_herjolfsnes.bindPopup("Herjolfsnes").openPopup();
+marker_herjolfsnes.on("click", clickOnHERJOLFSNES);
 
 
+// Evenements
 
-function clickOnCNAM(e){
+function clickOnCNAM(e) {
     alert("Vous visitez le musée et tombez sur une étrange carte !");
 }
-// LES MARQUEURS --- affichage evenements
+
+function clickOnROME(e) {
+    alert("ROMMMME");
+}
+
+function clickOnVENISE(e) {
+    alert("VENEZ, Venise ...");
+}
+
+function clickOnROSSLYN(e) {
+    alert("Grosse ligne");
+}
+
+function clickOnJERUSALEM(e) {
+    alert("JESUS SALEM");
+}
+
+function clickOnHERJOLFSNES(e) {
+    alert("HerWHAT §!?");
+}
+
+function clickOnNEWPORT(e) {
+    alert("vive l'usb");
+}
+
+
+
+
+// LES MARQUEURS --- affichage
 alert("Aujourd'hui a lieu une exposition sur les globes au CNAM");
 marker_cnam.addTo(mymap) // on ajoute le marqueur à la carte
+marker_rome.addTo(mymap);
+marker_venise.addTo(mymap);
+marker_rosslyn.addTo(mymap);
+marker_jerusalem.addTo(mymap);
+marker_newport.addTo(mymap);
+marker_herjolfsnes.addTo(mymap);
